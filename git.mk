a2faf7e5f09571f3af61dcf30e4afab1ed87b122
Roddy Shuler <roddy@endlessm.com>
2016-04-15 14:53:36 -0700

    Integrate Transifex translation infrastructure
    Include the results of the round-trip with Transifex,
    for a better baseline to compare against future translations.
    
    Note that we commit changes to the .pot to git.
    Since .gitignore is generated, this is a change to git.mk.

--- git.mk
+++ git.mk
@@ -148,7 +148,6 @@ $(srcdir)/.gitignore: Makefile.am $(top_srcdir)/git.mk
 				po/.intltool-merge-cache \
 				"po/*.gmo" \
 				"po/*.mo" \
-				po/$(GETTEXT_PACKAGE).pot \
 				intltool-extract.in \
 				intltool-merge.in \
 				intltool-update.in \
